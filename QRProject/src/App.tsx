2f4ebba21752c3e2739087f68ac62248dc3fdec9
Snowager <colepr24@gmail.com>
2024-12-07 14:20:27 -0700

    Refactor component design
    -makes component easier to modify
    -QRContainer generates random grid on component mount

--- QRProject/src/App.tsx
+++ QRProject/src/App.tsx
@@ -1,20 +1,26 @@
 //import { Dispatch, useState } from 'react'
+import { useMemo } from 'react'
 import './App.css'
 import QRContainer from './components/qrContainer'
 
 function App() {
-  //const [count, setCount]: [number, Dispatch<number>] = useState(0)
-  const cellWidth: number[] = [0,1,0,1,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,1,1,0,1,0,0,1,0,1,1,1]
+  
+  const ui = useMemo(() => {
+    return (
+      <>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <QRContainer cellWidth={40} cellHeight={40} cellAmountWidth={5} cellAmountHeight={6}/>
+          <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+        </div> 
+      </>
+    )
+  }, [])
 
   return (
     <>
-      <div style={{display: 'flex', flexDirection: 'column'}}>
-        <QRContainer containerWidth={600} containerHeight={'auto'} cellList={cellWidth} cellWidth={100} cellHeight={100}/>
-        <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      </div>
-      
+    {ui}
     </>
   )
 }
--- QRProject/src/components/qrContainer.tsx
+++ QRProject/src/components/qrContainer.tsx
@@ -1,22 +1,44 @@
-import { PropsWithChildren, ReactElement } from "react";
+import { Dispatch, PropsWithChildren, ReactElement, SetStateAction, useEffect, useMemo, useState } from "react";
 import QRCell from "./qrCell";
 
 interface qrProps {
-    containerWidth: number | string,
-    containerHeight: number | string, 
-    cellList: number[],
+    containerWidth?: number | string,
+    containerHeight?: number | string, 
+    cellList?: number[],
     cellWidth:number,
     cellHeight: number,
+    cellAmountWidth: number,
+    cellAmountHeight: number,
 }
 
-export default function QRContainer({containerWidth, containerHeight, cellList, cellWidth, cellHeight}: qrProps): ReactElement<PropsWithChildren> {
+export default function QRContainer({cellWidth, cellHeight, cellAmountWidth, cellAmountHeight}: qrProps): ReactElement<PropsWithChildren> {
+
+    //const [count, setCount]: [number, Dispatch<number>] = useState(0)
+    const [cellList, setCellList]: [number[], Dispatch<SetStateAction<number[]>>]= useState<number[]>([])
+
+    useEffect(() => {
+    const tempList: number[] = [];
+    while(tempList.length < 1) {
+        for (let x = 0; x < cellAmountWidth*cellAmountHeight; x++) {
+        const x = Math.floor(Math.random()*2)
+        tempList.push(x)
+        }
+        setCellList(tempList);
+    }
+    },[])
+
+    const QRui = useMemo(() => {
+        return (
+            <div style={{display: 'flex', flexWrap: 'wrap', alignItems: 'flex-start', flexDirection: 'row', width: cellWidth*cellAmountWidth, height: 'auto'
+            }}>
+                {cellList.length > 1 && cellList.map((cell) => {
+                    return (
+                    <QRCell cellWidth={cellWidth} cellHeight={cellHeight} toggleState={cell === 0 ? false : true}/>)})}
+            </div>
+        )
+    }, [cellList, cellWidth, cellHeight, cellAmountWidth])
     return (
-        <div style={{display: 'flex', flexWrap: 'wrap', alignItems: 'flex-start', flexDirection: 'row', width: containerWidth, height: containerHeight
-        }}>
-            {cellList && cellList.map((cell) => {
-                return (
-                <QRCell cellWidth={cellWidth} cellHeight={cellHeight} toggleState={cell === 0 ? false : true}/>)})}
-        </div>
+    <>{QRui}</>
     )
-}
+    }
 
