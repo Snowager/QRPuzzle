5e0c27d2b9f34b6ed755215f282e0853acbd5911
Snowager <colepr24@gmail.com>
2025-01-02 16:56:52 -0700

    Fix button placement

--- QRProject/src/components/rotateCubeContainer.tsx
+++ QRProject/src/components/rotateCubeContainer.tsx
@@ -14,78 +14,106 @@ export default function RotateCubeContainer({
     rightFace,
     backFace,
 }: cubeProps) {
-    const [deg, setDeg]= useState(0)
+    const [deg, setDeg] = useState(0);
     const width = 660;
-    const size = width/2;
-    
-
-    
-
-    
+    const size = width / 2;
 
     const ui = useMemo(() => {
         const styles: StyleSheet = {
             cube: {
                 WebkitTransformStyle: "preserve-3d",
-                transition: "transform 2s ease-in-out",
-                position: 'absolute',
+                transition: "transform .6s ease-in-out",
+                position: "relative",
                 WebkitTransform: `rotateY(${deg}deg)`,
                 height: width,
                 width: width,
-                left: 200,
-                top: 200,
             },
             backFace: {
                 WebkitTransform: `rotateY(180deg) translateZ(${size}px)`,
-                backgroundColor: 'blue',
+                backgroundColor: "white",
             },
             frontFace: {
                 WebkitTransform: `rotateY(0deg) translateZ(${size}px)`,
-                backgroundColor: 'red',
+                backgroundColor: "white",
             },
             leftFace: {
                 WebkitTransform: `rotateY(270deg) translateZ(${size}px)`,
-                backgroundColor: 'green',
+                backgroundColor: "white",
             },
             rightFace: {
                 WebkitTransform: `rotateY(90deg) translateZ(${size}px)`,
-                backgroundColor: 'yellow',
+                backgroundColor: "white",
             },
             face: {
-                position: 'absolute',
+                position: "absolute",
                 height: width,
                 width: width,
-            }
+            },
         };
 
-        function rotateRight() {
-            setDeg((value) => value+90)
-            console.log(deg)
-            console.log(JSON.stringify(styles))
+        function rotateLeft() {
+            setDeg((value) => value - 90);
         }
-        
+
+        function rotateRight() {
+            setDeg((value) => value + 90);
+        }
+
         return (
-            
-        <>
-        <div style={{display: 'flex', flexDirection: 'column', height: "100%", width: "100%"}}>
-            <div style={{...styles.cube}}>
-                <div style={{...styles.backFace, ...styles.face}}>{backFace}</div>
-                <div style={{...styles.leftFace, ...styles.face, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>{leftFace}</div>
-                <div style={{...styles.frontFace, ...styles.face}}>{frontFace}</div>
-                <div style={{...styles.rightFace, ...styles.face}}>{rightFace}</div>
-            </div>
-            <button onClick={() => rotateRight()} style={{width: 60, height: '60', position: 'relative'}}>Stuff</button>
-        </div>
-        </>
-        )
-    }, [backFace, frontFace, leftFace, rightFace, deg, size])
+            <>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        height: 660,
+                        width: 800,
+                        justifyContent: "space-evenly",
+                    }}
+                >
+                    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', paddingRight: 20, zIndex: 10}}>
+                        <button
+                            onClick={() => rotateRight()}
+                            style={{ height: 60, position: "relative" }}
+                        >
+                            RotateLeft
+                        </button>
+                    </div>
+                    <div style={{width: 660, height: 660}}>
+                    <div style={{ ...styles.cube }}>
+                        <div style={{ ...styles.backFace, ...styles.face }}>
+                            {backFace}
+                        </div>
+                        <div
+                            style={{
+                                ...styles.leftFace,
+                                ...styles.face,
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            {leftFace}
+                        </div>
+                        <div style={{ ...styles.frontFace, ...styles.face }}>
+                            {frontFace}
+                        </div>
+                        <div style={{ ...styles.rightFace, ...styles.face }}>
+                            {rightFace}
+                        </div>
+                    </div>
+                    </div>
+                    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', paddingLeft: 20}}>
+                        <button
+                            onClick={() => rotateLeft()}
+                            style={{ height: 60, position: "relative" }}
+                        >
+                            RotateRight
+                        </button>
+                    </div>
+                </div>
+            </>
+        );
+    }, [backFace, frontFace, leftFace, rightFace, deg, size]);
 
-
-    return (
-        <>
-        {ui}
-        </>
-    );
+    return <>{ui}</>;
 }
-
-
